02de63b9343c80d7e7398c98deaebc13bbc2bf17
Sang Nguyen <sangnguyenplus@gmail.com>
2026-04-11 16:04:06 +0700

    Improve docs

--- docs/orisa/sidebar.ts
+++ docs/orisa/sidebar.ts
@@ -3,24 +3,56 @@ import { DefaultTheme } from 'vitepress'
 export default [
   { text: 'Overview', link: '/orisa/' },
   {
-    text: 'Usage',
+    text: 'Prologue',
     items: [
-      { text: 'Homepage', link: '/orisa/usage-homepage' },
-      { text: 'Menus', link: '/orisa/usage-menus' },
-      { text: 'Theme Options', link: '/orisa/usage-theme-options' },
-      { text: 'UI Blocks', link: '/orisa/usage-ui-block' },
-      { text: 'Widgets', link: '/orisa/usage-widgets' },
-      { text: 'Custom CSS / JS', link: '/orisa/usage-custom-css-js' },
-      { text: 'Google Analytics', link: '/orisa/usage-analytics' },
+      { text: 'Release Notes', link: '/orisa/releases' },
+      { text: 'Upgrade Guide', link: '/orisa/upgrade' },
     ],
   },
   {
-    text: 'Maintenance',
+    text: 'Getting Started',
     items: [
-      { text: 'Upgrade Guide', link: '/orisa/upgrade' },
+      {
+        text: 'Installation',
+        items: [
+          { text: 'Requirements', link: '/orisa/installation-requirements' },
+          { text: 'Using web interface', link: '/orisa/installation-web-interface' },
+          { text: 'Using command line', link: '/orisa/installation-command-line' },
+          { text: 'Using Docker', link: '/orisa/installation-using-docker' },
+        ],
+      },
       { text: 'SSL', link: '/orisa/ssl' },
       { text: 'License', link: '/orisa/license' },
-      { text: 'Release Notes', link: '/orisa/releases' },
     ],
   },
-] as DefaultTheme.SidebarItem[]
+  {
+    text: 'Usage',
+    items: [
+      { text: 'Setup homepage', link: '/orisa/usage-homepage' },
+      { text: 'Menu', link: '/orisa/usage-menus' },
+      { text: 'UI Blocks', link: '/orisa/usage-ui-block' },
+      { text: 'Theme options', link: '/orisa/usage-theme-options' },
+      { text: 'Widgets', link: '/orisa/usage-widgets' },
+      { text: 'Custom CSS/JS', link: '/orisa/usage-custom-css-js' },
+      { text: 'Translation', link: '/orisa/plugin-translation' },
+      { text: 'Multi-language', link: '/orisa/usage-multi-language' },
+      { text: 'Google Analytics', link: '/orisa/usage-analytics' },
+      { text: 'Media - Setup Amazon S3', link: '/orisa/usage-media-s3' },
+      { text: 'Media - Setup BunnyCDN', link: '/orisa/usage-media-bunnycdn' },
+      { text: 'Media - Setup Wasabi', link: '/orisa/usage-media-wasabi' },
+      { text: 'Setup email', link: '/orisa/usage-email' },
+      { text: 'Currencies', link: '/orisa/usage-currencies' },
+      { text: 'Setup social login', link: '/orisa/usage-social-login' },
+      { text: 'Location', link: '/orisa/usage-location' },
+      { text: 'Invoice template', link: '/orisa/invoice-template' },
+      { text: 'Setup cronjob', link: '/orisa/cronjob' },
+      { text: 'Newsletter', link: '/orisa/usage-newsletter' },
+    ],
+  },
+  {
+    text: 'Development',
+    items: [
+      { text: 'Rename theme', link: '/orisa/theme-rename' },
+    ],
+  },
+] satisfies DefaultTheme.SidebarItem[]
